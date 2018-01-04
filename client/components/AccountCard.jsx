7183e33aeee09609fcbf5390a8cd4484ac91fd6f
arun <arun.kirubarajan@gmail.com>
2018-01-03 23:00:07 -0500

    Add AccountCard component

---
+++ client/components/AccountCard.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import copy from 'copy-to-clipboard';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { Notification } from 'react-notification';
+
+const marginLeft = {
+  marginLeft: ".5em",
+};
+
+class AccountCard extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      email: this.props.email,
+      password: this.props.password,
+      notification: false,
+      message: ""
+    }
+
+    this.handleEmailButton = this.handleEmailButton.bind(this);
+    this.handlePasswordButton = this.handlePasswordButton.bind(this);
+  }
+
+  handleEmailButton() {
+    event.preventDefault();
+    copy(this.state.email);
+    this.setState({ notification: true, message: "Email copied to clipboard!" });
+  }
+
+  handlePasswordButton() {
+    event.preventDefault();
+    copy(this.state.password);
+    this.setState({ notification: true, message: "Password copied to clipboard!"});
+  }
+  
+  hideNotification() {
+    this.setState({ notification: false });
+  }
+    
+  render() {
+    return (
+      <Card>
+        <CardBody>
+          <CardTitle>{this.props.title}</CardTitle>
+          <CardText>{this.props.notes}</CardText>
+          <Button onClick={this.handleEmailButton}> Email </Button>
+          <Button onClick={this.handlePasswordButton} style={marginLeft}> Password</Button>
+        </CardBody>
+
+        <Notification
+          isActive={this.state.notification}
+          message={this.state.message}
+          activeClassName={".notification"}
+          onDismiss={this.hideNotification.bind(this)}
+        />
+      </Card>
+    );
+  }
+}
+
+export default AccountCard;
