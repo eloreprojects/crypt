d296997491fb27236e3f28b4787b88fe733f5a54
arun <arun.kirubarajan@gmail.com>
2018-01-04 15:55:11 -0500

    Add email routing to smtp server

--- server/smtp.js
+++ server/smtp.js
@@ -22,20 +22,28 @@ mailin.on('message', (connection, data, content) => {
   const recipient = data.headers.to;
   const from = data.headers.from.replace("<","&lt;").replace(">", "&gt;"); 
 
-  // TODO: GET USER THAT HAS EMAIL AS recipient
-  // TODO: SET to AS USER'S EMAIL
+  console.log("sent");
 
-  var mailOptions = {
-    from: '"Crypt Forwarding Bot" <noreply@getcrypt.co>', 
-    to: to, 
-    subject: subject,
-    html: `<p> this email is originally from: ${from} and was sent to ${recipient} </p> <hr> <br>` + 
-            html + " <hr> <br> <h5> This email was forwarded by the Crypt Bot. </h5>"
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
+  Accounts.findOne({ email: recipient }, (err, account) => {
+    if (err) {
+      console.log(err);
     }
+
+    User.findOne({ _id: account.user }, (err, user) => {
+      const mailOptions = {
+        to,
+        subject,
+        from: '"Crypt Forwarding Bot" <eloreprojects@gmail.com>',  
+        html: `<p> this email is originally from: ${from} and was sent to ${recipient} </p> <hr> <br>` + html + " <hr> <br> <h5> This email was forwarded by the Crypt Bot. </h5>"
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(error);
+        }
+    
+        console.log(info);
+      });
+    });
   });
 });
