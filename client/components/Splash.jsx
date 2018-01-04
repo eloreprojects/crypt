e7ce65183b3692ad5d9c066e1077ca9cc1aeda3f
arun <arun.kirubarajan@gmail.com>
2018-01-03 23:01:21 -0500

    Add Splash page content

---
+++ client/components/Splash.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {  Nav, NavItem, NavLink, Container, Row, Col, Jumbotron, Button } from 'reactstrap';
+import AccountCard from './AccountCard.jsx';
+
+const marginTop = {
+  marginTop: "1.5em",
+};
+
+const marginLeft = {
+  marginLeft: ".5em",
+};
+
+const Splash = () => (
+  <div>
+    <Container >
+      <Row>
+        <Col md="12"> 
+          <h1 style={marginTop} className="display-3">Crypt</h1>
+          <p className="lead"> A minimalist hyper-secure account manager. </p>
+          <hr className="my-2" />
+          <p>
+            Crypt secures all your internet identities by generating disposable emails to obfuscate
+            accounts through.
+          </p>
+          <p className="lead">
+            <Button color="info">Enter</Button>
+            <Button style={marginLeft} color="secondary">Read More</Button>
+          </p>
+          
+          <h2 style={marginTop}>Demo</h2>
+          <p className="lead"> Stored information is AES encrypted and only be decrypted by you -- not even the Crypt team has access.</p>
+          <Col md="4">
+            <AccountCard
+              notes="You can type notes here! Press the buttons to copy your information to the clipboard." 
+              title="Examples Account"
+              email="g8alg90v@getcrypt.co"
+              password="!ajsuf9v0xx@12$" />
+          </Col>
+        </Col>
+      </Row>
+    </Container>
+  </div>
+);
+
+export default Splash;
