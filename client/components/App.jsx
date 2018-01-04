662ce779205dc998ee8001dec8ddfac0a989f76b
arun <arun.kirubarajan@gmail.com>
2018-01-03 23:00:25 -0500

    Add router to entrypoint

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,48 +1,11 @@
 import React from 'react';
-import AccountList from './AccountList.jsx';
-import CreateAccount from './CreateAccount.jsx';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Splash from './Splash.jsx';
 
-export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      accounts: []
-    }
-  }
+const App = () => (
+  <BrowserRouter>
+    <Route path="/" component={Splash} />
+  </BrowserRouter>
+);
 
-  componentWillMount() {
-    fetch('/accounts')
-      .then(res => res.json())
-      .then(accounts => {
-        this.setState({
-          accounts
-        });
-      })
-  }
-
-  handleAddAccount(newAccount) {
-    fetch('/accounts', {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(newAccount)
-    })
-    .then(res => res.json())
-    .then(account => {
-      this.setState({
-        accounts: this.state.accounts.concat(account)
-      });
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>crypt</h1>
-        <AccountList accounts={this.state.accounts} />
-        <CreateAccount onAddAccount={this.handleAddAccount.bind(this)} />
-      </div>
-    );
-  }
-}
+export default App;
