2a7a9dec8da2c5ccf2b150e2a4503584c5baa275
arun <arun.kirubarajan@gmail.com>
2018-01-05 03:27:15 -0500

    Refactor Login and Signup forms into seperate components

--- client/components/Enter.jsx
+++ client/components/Enter.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, FormGroup, Label, Input, Form, FormText, Button } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import LoginForm from './LoginForm.jsx';
+import SignupForm from './SignupForm.jsx';
 
 const marginTop = {
   marginTop: "1.5em",
-};
-
-const marginLeft = {
-  marginLeft: ".5em",
 };
 
 class Enter extends Component {
@@ -18,47 +16,11 @@ class Enter extends Component {
         <hr className="my-2" />
         <Row>
           <Col md={5}>
-            <Form>
-              <h4 style={marginTop}> Login </h4>
-              <FormGroup style={marginTop} row>
-                <Label sm={4}>Email</Label>
-                <Col sm={8}>
-                  <Input type="email" name="email" placeholder="your master email" />
-                </Col>
-              </FormGroup>
-              <FormGroup row>
-                <Label sm={4}>Password</Label>
-                <Col sm={8}>
-                  <Input type="password" name="password" placeholder="your master password" />
-                  <Button style={marginTop} type="submit" outline color="info"> Submit </Button>       
-                </Col>
-              </FormGroup>         
-            </Form>
+            <LoginForm />
           </Col>
           <Col md={2} />
           <Col md={5}>
-            <Form>
-              <h4 style={marginTop}> Sign Up </h4>
-              <FormGroup style={marginTop} row>
-                <Label sm={4}>Email</Label>
-                <Col sm={8}>
-                  <Input type="email" name="email" placeholder="your master email" />
-                </Col>
-              </FormGroup>
-              <FormGroup row>
-                <Label sm={4}>Password</Label>
-                <Col sm={8}>
-                  <Input type="password" name="password" placeholder="your master password" />
-                </Col>
-              </FormGroup>
-              <FormGroup row>
-                <Label sm={4}>Confirm</Label>
-                <Col sm={8}>
-                  <Input type="confirm" name="confirm" placeholder="confirm your master password" />
-                  <Button style={marginTop} type="submit" outline color="info"> Submit </Button>                         
-                </Col>
-              </FormGroup>
-            </Form>
+            <SignupForm />
           </Col>
         </Row>
       </Container>
